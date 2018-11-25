6fc9870b52bb8ee82b63afd4b7b997dd1b6afb7f
grant smith <grantsmith1256@gmail.com>
2018-11-25 20:16:33 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   
   def execute_transaction
     binding.pry
-    if @sender.balance > @amount
+    if @sender.balance < @amount
     @status = "Transaction rejected. Please check your account balance."
     elsif @status == "pending"
     @sender.balance = @sender.balance - @amount
